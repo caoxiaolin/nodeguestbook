69da18b9c3165e43488a2535a3e1bf619d63707b
caoxiaolin <xmihu945@qq.com>
2016-09-07 06:47:25 +0800

    modify by caoxiaolin

--- config.js
+++ config.js
@@ -1,7 +1,4 @@
-exports.debug = true;
 exports.port = 3000;
-exports.session_secret = 'scott todo session secret';
-exports.root=__dirname;
 
-// mongoDb connection 
+// mongodb connection 
 exports.db = 'mongodb://127.0.0.1:27017/demo';
